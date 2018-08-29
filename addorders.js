9ead59be9494842545c709cdb5756320e2165dbf
lydstyl <lydstyl@gmail.com>
2018-08-29 09:44:38 +0200

    pass.json

--- addorders.js
+++ addorders.js
@@ -1,7 +1,25 @@
 const api = require('binance'); // https://www.npmjs.com/package/binance
+
+
+let fs = require("fs");
+let passjson = fs.readFileSync("pass.json");
+passjson = JSON.parse(passjson);
+// Get Value from JSON
+console.log("User Name:", passjson.password);
+console.log("Email:", passjson.key);
+console.log("Password:", passjson.secret);
+
 const binanceRest = new api.BinanceRest({
-    key: 'yourKey', // Get this from your account on binance.com
-    secret: 'yourSecret', // Same for this
+
+
+
+    // todo mettre les login et pass hached dans un json ? voir avec Alex
+
+
+
+
+    key: passjson.key, // Get this from your account on binance.com
+    secret: passjson.secret, // Same for this
     timeout: 15000, // Optional, defaults to 15000, is the request time out in milliseconds
     recvWindow: 10000, // Optional, defaults to 5000, increase if you're getting timestamp errors
     disableBeautification: false,
